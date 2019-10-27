504805dae66f38737eb85ac137298f0824b74214
dan <dan@localhost.localdomain>
2019-10-27 19:02:16 +0300

    create a form to add a new quote.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Goal } from './goal'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Goals';
+export class AppComponent implements OnInit {
+
+
+
+  ngOnInit(){
+
+  }
 }
